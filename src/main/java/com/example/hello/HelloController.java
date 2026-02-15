6ba70b29817922bff1e2dcc75537807703ec83fb
Pram821 <pram35009@gmail.com>
2026-02-15 03:44:44 -0600

    Revert "Add /hello2 GET endpoint"
    This reverts commit 39eef33f2d01a8a15276d1d91300526bdbd81d47.

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -10,9 +10,4 @@ public class HelloController {
     public String hello() {
         return "Hello, Spring Boot!";
     }
-
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "Hello 2nd";
-    }
 }
